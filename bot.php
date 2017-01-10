a21390e41475f0b237a22b5717d6ff03af4da54f
AAommy <sureerat.aom1504@gmail.com>
2017-01-10 14:37:54 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -24,7 +24,11 @@ if (!is_null($events['events'])) {
 			}
 			
 			$messages = [
-				'type' => 'image',
+				"type": "location",
+				"title": "my location",
+				"address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+				"latitude": 35.65910807942215,
+				"longitude": 139.70372892916203
 				//'text' => $Textreply
 			];
 
