18ea1b7b97555b4ae852b2eaecc95edec0839631
AAommy <sureerat.aom1504@gmail.com>
2017-01-10 15:38:39 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 			$messages = [
 				'type' => 'sticker',
 				'packageId' => '1',
-				'stickerId' => '1'
+				'stickerId' => '3'
 			];
 			
 			/*$messages = [
