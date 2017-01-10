d7ac019c4bb976bfa2a3d49979c3a135020eafbc
AommySureerat <sureerat.aom1504@gmail.com>
2017-01-10 11:11:32 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -17,10 +17,17 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => 'aaaa'
-			];
+			if($text == 'hi'){
+				$messages = [
+					'type' => 'text',
+					'text' => 'hello'
+				];
+			}else{
+				$messages = [
+					'type' => 'text',
+					'text' => '555'
+				];
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
