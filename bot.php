e5102eebbba621b4de62dbfcb5df2f0f1313d48b
AAommy <sureerat.aom1504@gmail.com>
2017-01-11 09:51:35 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -31,7 +31,7 @@ if (!is_null($events['events'])) {
 				$messages = [
 					'type' => 'image',
 					'originalContentUrl' => 'https://upload.wikimedia.org/wikipedia/commons/b/b4/JPEG_example_JPG_RIP_100.jpg',
-					'previewImageUrl' => 'https://github.com/AAommy/LINE-BOT-PHP-Starter/blob/master/aaa.jpg'
+					'previewImageUrl' => 'http://i825.photobucket.com/albums/zz180/x3shanerx3/Buttons/button-1-1.jpg'
 				];
 			}
 			/*$messages = [
