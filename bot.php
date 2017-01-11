ba90c357465848b22d49c7b5423aa9bfd0f44df9
AAommy <sureerat.aom1504@gmail.com>
2017-01-11 17:06:06 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -60,16 +60,6 @@ if (!is_null($events['events'])) {
 					'text' => '555'
 				];
 			}
-			/*$messages = [
-				'type' => 'text',
-				'text' => $Textreply
-			];*/
-			
-			/*$messages = [
-				'type' => 'image',
-				'originalContentUrl' => 'Hydrangeas.jpg',
-				'previewImageUrl' => 'aaa.jpg'
-			];*/
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
