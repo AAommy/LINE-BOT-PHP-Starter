198f1de667fc3042cf0b2445adfe039b603af9be
AAommy <sureerat.aom1504@gmail.com>
2017-01-12 10:48:21 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -17,19 +17,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'template',
-				'altText' => 'this is a buttons template',
-				'template' => {
-					'type' => 'buttons',
-					'thumbnailImageUrl' => 'https://upload.wikimedia.org/wikipedia/en/6/6d/Pullinger-150x150.jpg',
-					'title' => 'Menu',
-					'text' => 'Please select',
-					'actions' => {
-							'type' => 'postback',
-							'label' => 'Buy',
-							'data' => 'action=buy&itemid=123'
-					}
-				}
+				'type' => 'text',
+				'text' => 'hello'
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
