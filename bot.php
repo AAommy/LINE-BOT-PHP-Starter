8c6d61a789d9ce3f9085412ccec7eca26ac43702
AAommy <sureerat.aom1504@gmail.com>
2017-01-12 11:12:49 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -17,14 +17,12 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages[] = [
-			{
 				'type' => 'text',
-				'text' => 'Helloooo'
-			},
-			{
+				'text' => 'Hello'
+			];
+			$messages[] = [
 				'type' => 'text',
 				'text' => 'How are you?'
-			}
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
