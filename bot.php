fb35027145b848be8a803c0cec253e9934a83d6d
AAommy <sureerat.aom1504@gmail.com>
2017-01-12 10:35:01 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,7 +1,8 @@
 <?php
 $access_token = 'wzwpbz9tZWCSPDrTFYf+APzByZ3jnlV259OV13WiCcsBXMftEVvi/OzVdEy8C31CYj4iA6GdPwQ5QCBnrJPKTNC4IcxZlr4bJwIVRAPd1FlWnDG8ThGjHWY4ZIOD1V/DhshZVuUJUv+YfDrLgh6xtgdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
-$content = file_get_contents('php://input');
+//$content = file_get_contents('php://input');
+$content = {"events":[{"type":"message","replyToken":"9a0353fad2ca47009bd42d4716116bb8","source":{"userId":"Ubb0233685f6c43ad7af9f72476d67f16","type":"user"},"timestamp":1484190639335,"message":{"type":"text","id":"5489547671801","text":"p"}}]};
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
@@ -55,3 +56,4 @@ if (!is_null($events['events'])) {
 	}
 }
 echo 'OK';
+print_r($events);
