0164f85bab5136ed449a62d26253769ad56bf5b8
AAommy <sureerat.aom1504@gmail.com>
2017-01-17 14:21:59 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -73,7 +73,7 @@ if (!is_null($events['events'])) {
 			
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [$messages[]],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
